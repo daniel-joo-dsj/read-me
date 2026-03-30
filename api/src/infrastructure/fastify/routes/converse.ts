0edbeff586251ebea0bf609794802c3141a07599
Daniel Joo <danieljoo@Daniels-MacBook-Pro.local>
2026-03-30 15:09:24 -0400

    Remove comments

--- api/src/infrastructure/fastify/routes/converse.ts
+++ api/src/infrastructure/fastify/routes/converse.ts
@@ -4,15 +4,11 @@ import type { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyReques
 async function converse (fastify: FastifyInstance, options: FastifyPluginOptions) {
     const { conversationAdapter } = options;
     fastify.post('/converse', async (request : FastifyRequest, reply: FastifyReply) => {
-        // create conversation id
         await conversationAdapter.createConversation();
         return reply.status(201).send('Conversation created successfully.')
     })
 
     fastify.post('/converse/:conversation_id/messages', async (request : FastifyRequest, reply: FastifyReply) => {
-        // pass in agent as a prop
-        // save message in sql with conversation_id
-        // get conversation history
 
     })
 }
